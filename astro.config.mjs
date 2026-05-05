b06b2c192ef1d6ad72d7a9791f4ce06f2688692d
Rocio Heredia <rocioheredia264@gmail.com>
2026-05-04 23:27:24 -0300

    Configurar base para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
-  output: 'static',
   base: '/bar-1592',
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
