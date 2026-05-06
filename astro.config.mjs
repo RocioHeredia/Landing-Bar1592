8c96555c21af95d03a8fc42ce72e55438fc5d5d5
Rocio Heredia <rocioheredia264@gmail.com>
2026-05-06 09:41:43 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
-  base: '/landing-bar1592',
+  //base: '/Bar1592',
   vite: {
     plugins: [tailwindcss()],
   },
